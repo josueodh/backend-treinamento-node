cbc18ab19daf6b26bb1f453a9fb5dd9645baa2cb
Josué Heringer <josuedelgadoheringer98@gmail.com>
2020-12-03 10:21:36 -0300

    enviando rota de login

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 
+import AuthenticateUserService from '../services/AuthenticateUserService';
 import CreateUserService from '../services/CreateUserService';
+import User from '../models/User';
 
 const usersRouter = Router();
 
@@ -23,5 +26,39 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.post('/login', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({ email });
+
+        if (!user) {
+            const createUser = new CreateUserService();
+
+            const user = await createUser.execute({
+                name, email, password
+            })
+        } else {
+
+            const authenticateUser = new AuthenticateUserService();
+
+            const { user, token } = await authenticateUser.execute({
+                email,
+                password
+            });
+
+        }
+
+        delete user.password;
+
+        return response.status(200).json(user);
+
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+})
 
 export default usersRouter;
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,11 +1,14 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+
 import User from '../models/User';
+
 interface Request {
     name: string;
     email: string;
     password: string;
 }
+
 class CreateUserService {
     public async execute({ name, email, password }: Request): Promise<User> {
         const usersReposity = getRepository(User);
