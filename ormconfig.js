1f918f0e0659de69eb8dc4bf5e138113b9c7b8a3
Josué Heringer <josuedelgadoheringer98@gmail.com>
2020-12-07 12:22:48 -0300

    removendo pack

--- ormconfig.js
+++ ormconfig.js
@@ -2,10 +2,10 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "entities": [
-        ".dist/src/models/**/*.js"
+        "./dist/src/models/**/*.js"
     ],
     "migrations": [
-        ".dist/src/database/migrations/*.js"
+        "./dist/src/database/migrations/*.js"
     ],
     "cli": {
         "migrationsDir": [
